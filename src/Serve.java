5603f9b33977f40f50628aa116fc5473c853e330
Dhaval Chavada <chavadadhaval206@gmail.com>
2020-11-13 23:32:28 -0500

    Added support for multiple packets

--- src/Serve.java
+++ src/Serve.java
@@ -3,22 +3,25 @@ import model.Packet;
 import model.ServerParameters;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Serve extends Thread {
     SocketAddress socketAddress;
-    ByteBuffer buf;
+    ArrayList<Packet> packetsReceived;
 
-    public Serve(SocketAddress socketAddress, ByteBuffer buf) {
+    public Serve(SocketAddress socketAddress, ArrayList<Packet> packetsReceived) {
         this.socketAddress = socketAddress;
-        this.buf = buf;
+        this.packetsReceived = packetsReceived;
     }
 
     @Override
@@ -32,19 +35,25 @@ public class Serve extends Thread {
 //            StringBuilder sb = new StringBuilder();
 
             // Parse a packet from the received raw data.
-            buf.flip();
-            Packet packet = Packet.fromBuffer(buf);
-            buf.flip();
+//            System.out.println("Limit:" + buf.limit());
+
+            String response = "";
+
+            for (Packet each : packetsReceived) {
+                System.out.println("Received Packet " + each.getSequenceNumber());
+                String responsePayload = new String(each.getPayload(), StandardCharsets.UTF_8);
+                response = response.concat(responsePayload.trim());
+            }
 
             //Perform the operations on received request...
             if (Config.isVerbose)
-                System.out.println("Incoming Connection:  " + packet.getPeerAddress());
+                System.out.println("Incoming Connection:  " + packetsReceived.get(0).getPeerAddress());
 
             ServerParameters serverParameters = new ServerParameters();
-            String payload = new String(packet.getPayload(), UTF_8);
-            httpRequest.processRequest(new StringBuilder(payload), serverParameters);
 
-            System.out.println("Peer port:" + packet.getPeerPort());
+            ArrayList<Packet> packetList = generatePackets(httpRequest, serverParameters, packetsReceived.get(0), response);
+
+            System.out.println("Peer port:" + packetsReceived.get(0).getPeerPort());
 
 //                            logger.info("Packet: {}", packet);
 //                            logger.info("Payload: {}", payload);
@@ -54,12 +63,17 @@ public class Serve extends Thread {
             // The peer address of the packet is the address of the client already.
             // We can use toBuilder to copy properties of the current packet.
             // This demonstrate how to create a new packet from an existing packet.
-            Packet responsePacket = packet.toBuilder()
-                    .setPayload(serverParameters.response.getBytes())
-                    .create();
-            channel.send(responsePacket.toBuffer(), socketAddress);
-            buf.clear();
+//            Packet responsePacket = packet.toBuilder()
+//                    .setPayload(serverParameters.response.getBytes())
+//                    .create();
 
+            for (Packet each : packetList) {
+                channel.send(each.toBuffer(), socketAddress);
+                System.out.println("Packet Sent as Response");
+            }
+
+//            buf.clear();
+            packetList.clear();
 //            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 //            ServerParameters serverParameters = new ServerParameters();
 //            while (byteBuffer.hasRemaining()) {
@@ -91,7 +105,53 @@ public class Serve extends Thread {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
 
+    private static ArrayList<Packet> generatePackets(HttpRequest httpRequest, ServerParameters serverParameters, Packet packet, String payload) {
+        ArrayList<Packet> packetList = new ArrayList<>();
+//        String payload = httpRequest.processRequest(serverParameters);
+        httpRequest.processRequest(new StringBuilder(payload), serverParameters);
+        byte[] buffer = serverParameters.response.getBytes();
+        long seq = 1L;
+        int i = 0;
 
+//        for (int i = 0; i < buffer.length; i = i + 1013)
+        while (i < buffer.length){
+//            byte[] bytes = payload.getBytes(i, i + 1012);
+            System.out.println("I:" + i + " I + 1012:" + (i + 1012) + " buffer Length:" + buffer.length);
+            byte[] slice;
+            if (i + 1012 > buffer.length) {
+                slice = Arrays.copyOfRange(buffer, i, buffer.length - 1);
+            } else {
+                slice = Arrays.copyOfRange(buffer, i, i + 1012);
+            }
+
+            System.out.println("SLICE length:" + slice.length);
+
+            Packet responsePacket = packet.toBuilder()
+                    .setSequenceNumber(seq)
+                    .setPayload(slice)
+                    .create();
+
+            packetList.add(responsePacket);
+            seq++;
+            i += 1013;
+        }
+
+//        i -= 1012;
+//
+//        if ((buffer.length - i) > 0) {
+//            System.out.println("i:" + i + " buffer Length:" + (buffer.length - 1));
+//            byte[] slice = Arrays.copyOfRange(buffer, i, i + (buffer.length - 1));
+//
+//            Packet responsePacket = packet.toBuilder()
+//                    .setSequenceNumber(seq)
+//                    .setPayload(slice)
+//                    .create();
+//
+//            packetList.add(responsePacket);
+//        }
+
+        return packetList;
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -7,13 +7,15 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.channels.DatagramChannel;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
+import static java.nio.channels.SelectionKey.OP_READ;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Server {
@@ -36,15 +38,59 @@ public class Server {
                                 .order(ByteOrder.BIG_ENDIAN);
                         buf.clear();
 
+                        channel.configureBlocking(false);
+                        Selector selector = Selector.open();
                         // TO-DO Handle multiple packets by multiple clients
-
                         while (true) {
-                            SocketAddress router = channel.receive(buf);
+                            SocketAddress router = null;
+                            ArrayList<Packet> packetList = new ArrayList<>();
 
-                            System.out.println("Length:" + buf.limit());
+                            // Receive a packet within the timeout
+                            channel.register(selector, OP_READ);
 
-                            Serve serve = new Serve(router, buf);
+                            selector.select();
+                            Set<SelectionKey> keys = selector.selectedKeys();
+                            System.out.println("Selector Keys Size:" + keys.size());
+                            Iterator<SelectionKey> iterator = keys.iterator();
+                            SelectionKey key = iterator.next();
+
+                            while (key.isReadable()) {
+//                                if (selectionKey.isReadable()) {
+                                if (router == null)
+                                    router = channel.receive(buf);
+                                else
+                                    channel.receive(buf);
+
+                                buf.flip();
+
+                                if (buf.limit() < Packet.MIN_LEN || buf.limit() > Packet.MAX_LEN)
+                                    break;
+
+                                Packet responsePacket = Packet.fromBuffer(buf);
+                                buf.clear();
+                                packetList.add(responsePacket);
+                                System.out.println("Received Arraylist Size:" + packetList.size());
+//                                System.out.println("Length:" + buf.limit());
+//                                }
+//                                // Wait for response
+//                                selector.select(5000);
+//
+//                                keys = selector.selectedKeys();
+//                                if (keys.isEmpty()) {
+//                                    System.out.println("No response after timeout");
+//                                    keys.clear();
+//                                    break;
+//                                }
+                            }
+
+//                            String response = "";
+
+                            ArrayList<Packet> copy = new ArrayList<>(packetList);
+                            Serve serve = new Serve(router, copy);
                             serve.start();
+//                            serve.join();
+                            packetList.clear();
+                            buf.clear();
                         }
                     } catch (IOException ex) {
                         ex.printStackTrace();
